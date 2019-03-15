8a7494e9342ac781c8c6200271de427a73f808b7
Alain Davis <davisbedetse@gmail.com>
2019-03-15 19:28:36 -0400

    final version

--- src/App.js
+++ src/App.js
@@ -7,22 +7,36 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.handleStart = this.handleStart.bind(this);
+    this.handleReplay = this.handleReplay.bind(this);
+
     this.state = {
-      randomCell: 0
+      randomCell: Math.floor(Math.random() * 9) + 1
     };
   }
   handleStart() {
     //Generate random between 1 and 9
-
     this.setState({ randomCell: Math.floor(Math.random() * 9) + 1 });
+  }
+
+  handleReplay() {
+    window.location.reload();
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Mine sweep</h1>
+        <h1 id="title">Mine sweep</h1>
+        <p id="intro" className="font-weight-bolder">
+          A mine was planted in one of the cells below.Clear the other cells to
+          win.
+        </p>
         <Board minedCell={this.state.randomCell} />
-        <button onClick={this.handleStart}>Start Game</button>
+        <button className="btn btn-success " onClick={this.handleStart}>
+          Start Game
+        </button>
+        <button className="btn btn-warning " onClick={this.handleReplay}>
+          Replay
+        </button>
       </div>
     );
   }
--- src/components/board.jsx
+++ src/components/board.jsx
@@ -10,26 +10,39 @@ export class Board extends Component {
   };
 
   green_cell = {
-    backgroundColor: "green",
+    backgroundColor: "important green",
     height: "50px",
     width: "50px"
   };
+
+  styles = this.red_cell;
+
+  cellArray = [];
 
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
     this.state = {
-      styles: this.red_cell,
       clicked: 1,
-      currentStatus: "Keep playing..."
+      currentStatus: " ",
+      score: 0,
+      win: ""
     };
   }
 
   handleClick(e) {
-    this.setState({ clicked: e.target.id });
+    // eslint-disable-next-line
     if (this.props.minedCell != e.target.id) {
-      //this.setState({ styles: this.green_cell });
       e.target.style.backgroundColor = "green";
+      this.cellArray.push(e.target.id);
+      console.log("cellArray:" + this.cellArray);
+      console.log("cellArray length:" + this.cellArray.length);
+      this.setState({ score: this.state.score + 1 });
+
+      // eslint-disable-next-line
+      if (this.cellArray.length == 8) {
+        this.setState({ win: "you won!" });
+      }
     } else {
       this.setState({ currentStatus: "You blew up!" });
     }
@@ -38,41 +51,43 @@ export class Board extends Component {
   render() {
     return (
       <div className="d-flex flex-column">
-        <p>Cell number {this.props.minedCell} is mined</p>
-        <p>You clicked {this.state.clicked}</p>
-        <p>{this.state.currentStatus}</p>
+        <p className="gameStatus">{this.state.currentStatus}</p>
         <div id="firstRow">
-          <button id="1" style={this.state.styles} onClick={this.handleClick}>
+          <button id="1" style={this.styles} onClick={this.handleClick}>
             1
           </button>
-          <button id="2" style={this.state.styles} onClick={this.handleClick}>
+          <button id="2" style={this.styles} onClick={this.handleClick}>
             2
           </button>
-          <button id="3" style={this.state.styles} onClick={this.handleClick}>
+          <button id="3" style={this.styles} onClick={this.handleClick}>
             3
           </button>
         </div>
         <div id="secondRow">
-          <button id="4" style={this.state.styles} onClick={this.handleClick}>
+          <button id="4" style={this.styles} onClick={this.handleClick}>
             4
           </button>
-          <button id="5" style={this.state.styles} onClick={this.handleClick}>
+          <button id="5" style={this.styles} onClick={this.handleClick}>
             5
           </button>
-          <button id="6" style={this.state.styles} onClick={this.handleClick}>
+          <button id="6" style={this.styles} onClick={this.handleClick}>
             6
           </button>
         </div>
         <div id="thirdRow">
-          <button id="7" style={this.state.styles} onClick={this.handleClick}>
+          <button id="7" style={this.styles} onClick={this.handleClick}>
             7
           </button>
-          <button id="8" style={this.state.styles} onClick={this.handleClick}>
+          <button id="8" style={this.styles} onClick={this.handleClick}>
             8
           </button>
-          <button id="9" style={this.state.styles} onClick={this.handleClick}>
+          <button id="9" style={this.styles} onClick={this.handleClick}>
             9
           </button>
+          <p id="won" className="font-weight-bolder">
+            {this.state.win}
+          </p>
+          <p id="score">Score:{this.state.score}</p>
         </div>
       </div>
     );
